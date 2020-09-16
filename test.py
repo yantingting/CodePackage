91ceea7d9beaaed769d70412e7f027900603cc06
yantingting <git.mintq.com>
2020-09-16 14:25:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -25,6 +25,7 @@ if not os.path.exists(result_path):
 
 df = pd.read_excel(os.path.join(data_path, 'sample_data.xlsx'))
 df.head()
+df['flag'].sum()/df['flag'].count()
 
 # step 1:生成数据字典，方便后期管理建模可用的变量
 my_dict = DataSummary().create_dict(df , is_available = 'Y', data_sorce='TYC', data_type='三方', useless_vars = ['company_name_md5'])
@@ -34,6 +35,8 @@ my_dict
 my_eda= eda(df, useless_vars = ['company_name_md5'], special_value = [np.nan, 0], var_dict = my_dict, result_path = result_path, save_label = 'test', cutoff=0.9)
 
 # step3: 拆分数据集，随机分，分层随机分，按时间分
+
+
 
 
 
